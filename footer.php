583d11d6a98f1529b64548e4601bb8ba6c6c464e
Termeh1378 <155793151+Termeh1378@users.noreply.github.com>
2024-01-05 19:57:39 -0800

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,13 @@
+</section>
+				</section>
+			</section>
+<footer class="divTable" style="text-align: center;">
+				<section class="divTableRow">
+					<article style="background-color: dimgray; color: white;" class="divTableCell"> &copy; تمام حقوق این سامانه برای <a href="#" style="text-decoration: none; font-weight: bold; color: aqua;">Ritma Online Store Of Electronic Products</a> محفوظ بوده و هرگونه کپی برداری پیگرد قانونی دارد.  </article>
+				</section>
+			</footer>
+		</div>
+	</div>
+</div>
+</body>
+</html>
